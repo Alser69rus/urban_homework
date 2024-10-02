ed57b6b2b0b2216f1ca808e861396ff6477c49e2
Алекс <evdokimov69rus@mail.ru>
2024-10-02 12:51:26 +0300

    02.10.24 rename 1_4 to 1_4xx and add new 1_4

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,8 @@
-number_of_completed_homework = 12
-number_of_hours_spent = 1.5
-course_name = 'Python'
-time_for_one_task =number_of_hours_spent  / number_of_completed_homework
-print('Курс: ', course_name, ' всего задач:', number_of_completed_homework, ' затрачено часов:',
-      number_of_hours_spent, ' среднее время выполнения:', time_for_one_task, 'часа.', )
+my_string = input('Введите что-нибудь: ')
+print('Введено символов:', len(my_string))
+print('Верхний регистр', my_string.upper())
+print('Нижний регистр', my_string.lower())
+my_string = my_string.replace(' ', '')
+print('Измененная строка без пробелов:', my_string)
+print('Первый символ:', my_string[0])
+print('Последний символ:', my_string[-1])
