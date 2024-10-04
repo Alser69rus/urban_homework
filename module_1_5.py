05f2829752cb87b03294a1696d69ac34c12cb09a
Алекс <evdokimov69rus@mail.ru>
2024-10-04 10:59:24 +0300

    02.10.24 rename 1_5 to 1_5xx and add new 1_5

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,12 @@
-# some comment
-example='Топинамбур'
-print(example[0])
-print(example[-1])
-print(example[len(example)//2:])
-print(example[::-1])
-print(example[1::2])
+immutable_var = 1, 2, "a", "b"
+print("Immutable tuple:", immutable_var)
+mutable_list = [1, 2, "a", "b", True]
+mutable_list[4] = "Modified"
+print("Mutable list:", mutable_list)
+
+try:
+    immutable_var[0] = True
+    print("Кортеж можно изменить")
+except Exception as e:
+    print("Кортеж менять нельзя!")
+    print("Будет ошибка выполнения:", e)
