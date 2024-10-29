7ba383e01b35d2832a00cb572aeac8b8443022f2
Алекс <evdokimov69rus@mail.ru>
2024-10-29 11:33:54 +0300

    модули

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(value1, value2):
+    if value2 == 0:
+        return "Ошибка"
+    else:
+        return value1 / value2
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+
+def divide(value1, value2):
+    if value2 == 0:
+        return inf
+    else:
+        return value1 / value2
