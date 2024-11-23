2ee0ed8d3d3aa4cde2cc28ccc623fa71c4a115cf
Алекс <evdokimov69rus@mail.ru>
2024-11-23 15:29:41 +0300

    кдассы

--- module_4_2.py
+++ module_4_2.py
@@ -1,3 +1,10 @@
+from math import sin
+from datetime import datetime
+
+print(sin(0))
+print(datetime.now())
+
+
 def test_function():
     def inner_function():
         print("Я, inner_function, в области видимости функции test_function")
---
+++ module_5_1.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name: str, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor: int):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+            return
+        for floor in range(new_floor):
+            print(floor + 1)
+
+
+if __name__ == "__main__":
+    h1 = House("ЖК Горский", 18)
+    h2 = House("Домик в деревне", 2)
+    h1.go_to(5)
+    h2.go_to(10)
